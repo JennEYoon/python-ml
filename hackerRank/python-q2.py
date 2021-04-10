a0929365da1866aa13f4961c9b259dca923c0448
Jennifer E. Yoon <jenneyoon@gmail.com>
2021-04-10 19:18:38 -0400

    Update python-q2.py

--- hackerRank/python-q2.py
+++ hackerRank/python-q2.py
@@ -18,5 +18,8 @@ if __name__ == '__main__':
 
         
 # Q: what is .strip()?  input strip out text format?  
+# A: Removes any leading and trailing white spaces.  In case input has white spaces.  
+# If .strip(chars) parameter specified, it will strip leading and trailing matching characters.  
+
 # Q why funciton called "if __name__ = '__main__': ?  Why dunders? 
 
