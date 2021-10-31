9e10bb98825a38cbfc7db90a358c767c7f85a05d
JennEYoon <jenneyoon@gmail.com>
2021-10-31 15:43:44 -0400

    Add folders python practice

---
+++ hackerrank/python-hacker-rank/py-counting-sort.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingSort function below.
+def countingSort(arr):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
+    
+   """
+   IO stream management for python 3.  
+   input is a files in directory 'OUTPUT_PATH' specified.
+   
+   """
