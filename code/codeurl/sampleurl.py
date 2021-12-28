287d234d9e18f2247fd2890e6192fc96a3ee53b1
JennEYoon <jenneyoon@gmail.com>
2021-12-28 17:11:31 -0500

    Move folders out to private repo

--- code/codeurl/sampleurl.py
+++
@@ -1,6 +0,0 @@
-# filename: sampleurl.py  
-# Sample python file for URL call from datasciY.com to private GitHub repo   
-# repo: jennifer-yoon, branch: main   
-
-print("Hello from #jennifer-yoon repo!")  
-print(python --version)
--- code/py-workout/python-workout-master/ch08-modules/screencasts/menu.py
+++
@@ -1,9 +0,0 @@
-def menu(**options):
-    while True:
-        option_string = '/'.join(sorted(options))
-        choice = input(f'Enter an option ({option_string}): ')
-
-        if choice in options:
-            return options[choice]()
-        else:
-            print(f'{choice} is not a valid option')
